7b9215658ce1e6240ecc9945b06cd8415e7d9fa9
Terrence Hosang <thosang@gmail.com>
2025-04-10 02:06:59 -0400

    media_with_cors override

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,6 +5,13 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
     SpectacularRedocView,
 )
+
+from django.views.static import serve as static_serve
+from django.views.decorators.http import require_GET
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from django.urls import re_path
+
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -32,3 +39,16 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT,
     )
+
+@require_GET
+@csrf_exempt
+def media_with_cors(request, path):
+    response = static_serve(request, path, document_root=settings.MEDIA_ROOT)
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Headers"] = "Origin, Content-Type, Accept"
+    response["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+    return response
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', media_with_cors),
+]
